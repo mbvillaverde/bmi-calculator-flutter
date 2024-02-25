fcc39d99801b24327ef1223817c3e0cee49da89b
Michael Villaverde <michaelv@qed.software>
2024-02-25 15:46:01 +0800

    done icon content components with gestures; 130 next

---
+++ lib/input_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'components/icon_content.dart';
+import 'components/reusable_card.dart';
+
+const double bottomContainerHeight = 80.0;
+const Color activeCardColor = Color(0xFF1D1E33);
+const Color inactiveCardColor = Color(0xFF111328);
+const Color defaultBottomButtonColor = Color(0xFFEB1555);
+
+enum Gender {
+  male,
+  female,
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  Gender? selectedGender;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    color: selectedGender == Gender.male
+                        ? activeCardColor
+                        : inactiveCardColor,
+                    cardChild: IconContent(
+                      contentIcon: FontAwesomeIcons.mars,
+                      contentText: 'MALE',
+                    ),
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    color: selectedGender == Gender.female
+                        ? activeCardColor
+                        : inactiveCardColor,
+                    cardChild: IconContent(
+                      contentIcon: FontAwesomeIcons.venus,
+                      contentText: 'FEMALE',
+                    ),
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(child: ReusableCard(color: activeCardColor)),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: ReusableCard(color: activeCardColor)),
+                Expanded(child: ReusableCard(color: activeCardColor)),
+              ],
+            ),
+          ),
+          Container(
+            color: defaultBottomButtonColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity, // value equal to full width of the screen
+            height: bottomContainerHeight,
+          ),
+        ],
+      ),
+    );
+  }
+}
